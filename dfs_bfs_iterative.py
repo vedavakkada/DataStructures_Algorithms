6124ed36c281ed13511a0a0530d538a6b191144e
vedavakkada <77647914+vedavakkada@users.noreply.github.com>
2025-03-16 20:55:42 -0400

    Update dfs_bfs_iterative.py

--- dfs_bfs_iterative.py
+++ dfs_bfs_iterative.py
@@ -1,1 +1,39 @@
+class TreeNode:
+    def __init__(self, node):
+        self.node = node
+        self.neighbors = []
 
+
+def bfs(graph):
+
+  visited = {graph.node}
+  queue = collections.deque()
+  queue.append(graph.neighbors)
+
+  while queue:
+
+    # remove first element
+    node = queue.popleft()
+    if node not in visited:
+      visited.add(node.node)
+      queue.append(node.neighbors)
+    
+  return visited
+
+def dfs(graph):
+
+  visited = {graph.node}
+  queue = collections.deque()
+  queue.append(graph.neighbors)
+
+  while queue:
+
+    # remove last element
+    node = queue.pop()
+    if node not in visited:
+      visited.add(node.node)
+      queue.append(node.neighbors)
+    
+  return visited
+    
+  
