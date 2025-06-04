29e2857247c746c4ea78cb03e82feb984e43e29f
vedavakkada <77647914+vedavakkada@users.noreply.github.com>
2025-06-04 19:42:56 -0400

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -12,4 +12,24 @@ def kadanes(nums):
     maxSum = max(currSum, maxSum)
 
   return maxSum
-  
+
+
+# sliding window with constant window
+def sliding(nums, k):
+    
+    seen = set()
+    
+    L = 0
+    n = len(nums)
+    for R in range(n):
+        
+        if R - L + 1 > k:
+            seen.remove(nums[L])
+            L += 1
+        
+        if nums[R] in seen:
+            return True
+            
+        seen.add(nums[R])
+            
+    return False
